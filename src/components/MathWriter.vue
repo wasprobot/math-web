a6aba481a0b56ba4b443e834ad21893a25c32e35
Rohit Wason <rwason1@humana.com>
2019-12-27 09:53:45 -0500

    for heroku

---
+++ src/components/MathWriter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>
+      <span id="answer" />
+    </p>
+  </div>
+</template>
+
+<script>
+import { getInterface } from "exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery!mathquill/build/mathquill.js";
+import "mathquill/build/mathquill.css";
+
+export default {
+  name: "mathwriter",
+  mounted() {
+    var MQ = getInterface(2);
+    MQ.MathField(document.getElementById("answer"));
+  }
+};
+</script>
